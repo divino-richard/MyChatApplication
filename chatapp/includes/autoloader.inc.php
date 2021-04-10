ddcefbeedc7e41351c2419ce564b04db621627fc
divino-richard <82223342+divino-richard@users.noreply.github.com>
2021-04-10 07:20:23 -0700

    Add files via upload

--- chatapp/includes/autoloader.inc.php
+++ chatapp/includes/autoloader.inc.php
@@ -1,1 +1,11 @@
+<?php
+    session_start();
+    spl_autoload_register('myAutoloader');//Run the autoloader function and pass-in the method created below  
 
+    function myAutoloader($className){//Autoload the flie when class name is called
+        $path = "classes/";
+        $extention = ".class.php";
+        $fullPath = $path . $className . $extention;
+
+        include_once $fullPath;
+    }
---
+++ chatapp/includes/header.inc.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS styles -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home Page</title>
+</head>
+<body>
+    <div class="container">
+            <div class="header">
+                <h1>CHATAPP</h1>
+                <div class="nav">
+                    <ul>
+                        <li>
+                            <a href="index.php"><img src="img/icons/home.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="img/icons/image.png" alt=""></a>
+                        </li>
+                        <li>
+                            <span>10</span>
+                            <a href="#"><img src="img/icons/message.png" alt=""></a>
+                        </li>
+                        <li>
+                            <a href="logout.php"><img src="img/icons/logout.png" alt=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
